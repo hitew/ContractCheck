f209bc1a53ea73bc0c69805ff6355eb031ce4e96
王希特 <53168636+hitew@users.noreply.github.com>
2024-05-10 16:10:41 +0800

    Update normal_ast.py

--- AFU_Extracted/normal_ast.py
+++ AFU_Extracted/normal_ast.py
@@ -3,225 +3,130 @@ import re
 import os
 from tqdm import tqdm
 
-class Statement_Norm(object):
+class Statement_Normalizer(object):
 
-    def __init__(self, statement_tokens_path):
+    def __init__(self, doc_token_dir):
 
-        self.statement_tokens_path = statement_tokens_path
-        # self.header_list = self.get_header_list()
-        # self.statement_list = self.get_statement_list()
+        self.doc_token_dir = doc_token_dir
         self.linespan_list, \
         self.ancestor_list, \
-        self.statement_list = self.load_statement_tokens()
-        self.statement_tokens = self.get_statement_tokens()
-        self.statement_tokens_type = self.get_statement_tokens_type()
-        self.statement_tokens_norm = self.get_statement_tokens_norm()
-        # print self.statement_tokens_norm
-        assert len(self.linespan_list) == len(self.ancestor_list)
-        assert len(self.ancestor_list) == len(self.statement_list)
-        assert len(self.statement_list) == len(self.statement_tokens_norm)
-        assert len(self.statement_tokens) == len(self.statement_tokens_type)
+        self.doc_list = self.load_document_tokens()
+        self.doc_tokens = self.extract_doc_tokens()
+        self.doc_tokens_type = self.extract_doc_tokens_type()
+        self.doc_tokens_norm = self.normalize_doc_tokens()
 
-    def load_statement_tokens(self):
-        '''
-        '''
+        assert len(self.linespan_list) == len(self.ancestor_list)
+        assert len(self.ancestor_list) == len(self.doc_list)
+        assert len(self.doc_list) == len(self.doc_tokens_norm)
+        assert len(self.doc_tokens) == len(self.doc_tokens_type)
+
+    def load_document_tokens(self):
         linespan_list = []
         ancestor_list = []
-        statement_list = []
-        with open(self.statement_tokens_path ) as st:
-            for i, line in enumerate(st):
+        doc_list = []
+        with open(self.doc_token_dir ) as dt:
+            for i, line in enumerate(dt):
                 linespan = line.split('\t')[0].strip('[').strip(']').split(', ')
                 ancestor = line.split('\t')[1].strip('[').strip(']').split(', ')
-                statement = line.split('\t')[2]
-                if statement.startswith('[['):
+                doc = line.split('\t')[2]
+                if doc.startswith('[['):
                     linespan_list.append(linespan)
                     ancestor_list.append(ancestor)
-                    statement_list.append(statement.strip())
+                    doc_list.append(doc.strip())
                 else:
                     continue
-        return linespan_list, ancestor_list, statement_list
-        pass
+        return linespan_list, ancestor_list, doc_list
 
-    def get_statement_tokens(self):
-        '''
-        '''
-        statement_tokens = []
-        for statement_str in self.statement_list:
-            tokens = self.get_tokens(statement_str)
-            statement_tokens.append(tokens)
-        return statement_tokens
-        pass
+    def extract_doc_tokens(self):
+        doc_tokens = []
+        for doc in self.doc_list:
+            tokens = self.get_tokens_from_doc(doc)
+            doc_tokens.append(tokens)
+        return doc_tokens
 
-    def get_statement_tokens_type(self):
-        '''
-        '''
-        statement_tokens_type = []
-        for statement_str in self.statement_list:
-            tokens_type = self.get_token_type(statement_str)
-            statement_tokens_type.append(tokens_type)
-        return statement_tokens_type
-        pass
+    def extract_doc_tokens_type(self):
+        doc_tokens_type = []
+        for doc_str in self.doc_list:
+            tokens_type = self.get_token_types(doc_str)
+            doc_tokens_type.append(tokens_type)
+        return doc_tokens_type
 
-    def get_statement_tokens_norm(self):
-        statement_tokens_norm = []
-        for i in range(len(self.statement_list)):
-            token = self.statement_tokens[i]
-            token_type = self.statement_tokens_type[i]
+    def normalize_doc_tokens(self):
+        doc_tokens_norm = []
+        for i in range(len(self.doc_list)):
+            token = self.doc_tokens[i]
+            token_type = self.doc_tokens_type[i]
             assert len(token) == len(token_type)
-            tokens_norm = self.get_token_normalize(token, token_type)
-            statement_tokens_norm.append(tokens_norm)
-        return statement_tokens_norm
-        pass
+            tokens_norm = self.normalize_tokens(token, token_type)
+            doc_tokens_norm.append(tokens_norm)
+        return doc_tokens_norm
 
     def store(self):
         store_path = './AFU_Extracted/output/norm/'
-        sol = os.path.basename(self.statement_tokens_path)
-        with open(store_path + sol + '_normalized', 'w') as snt:
-            for e in zip(self.linespan_list, self.ancestor_list, self.statement_tokens_norm):
-                # snt.write( str(self.statement_tokens_path) )
-                snt.write('_'.join(e[0]) + '\t')
-                snt.write(' '.join(e[1]) + ' ')
-                snt.write(' '.join(e[2]) + '\n')
-        pass
+        sol = os.path.basename(self.doc_token_dir)
+        with open(store_path + sol + '_normalized', 'w') as sdt:
+            for e in zip(self.linespan_list, self.ancestor_list, self.doc_tokens_norm):
+                sdt.write('_'.join(e[0]) + '\t')
+                sdt.write(' '.join(e[1]) + ' ')
+                sdt.write(' '.join(e[2]) + '\n')
 
-    def get_tokens(self, statement_str, verbose=False):
-        '''
-            get token list form the statement_tokens
-            Parameters : statement_tokens (string)
-            return : list of tokens str
-        '''
-        tokens = []
+    @staticmethod
+    def get_tokens_from_doc(statement_str):
         token_clean_exp = '@[0-9]+,[0-9]+:[0-9]+=' + '|' \
-                                                     ',<[0-9]+>,[0-9]+:[0-9]+' + '|' \
-                                                                                 ',<[0-9]+>,channel=[0-9],[0-9]+:[0-9]+' + '|' \
-                                                                                                                           ',<-1>,[0-9]+:[0-9]+'
+                                                     ',<[0-9]+>,[0-9]+:[0-9]+' + '|',<[0-9]+>,channel=[0-9],[0-9]+:[0-9]+' + '|',<-1>,[0-9]+:[0-9]+'
         token_clean_reg = re.compile(token_clean_exp)
         token_clean = re.sub(token_clean_reg, '', statement_str)
         tokens = token_clean.strip().strip('[').strip(']').split('], [')
-        tokens = [e.strip("'") for e in tokens]
-        if verbose:
-            print(tokens)
+        return [e.strip("'") for e in tokens]
 
-        return tokens
-
-    def get_token_type(self, statement_str, verbose=False):
-        '''
-            get token type from the statement_tokens
-            Parameters : statement_tokens (string)
-            return : token_type, list of token types
-        '''
+    @staticmethod
+    def get_token_types(statement_str):
         token_type_exp = ',<[0-9]+>,|,<-1>,'
         token_type_reg = re.compile(token_type_exp)
         token_type = token_type_reg.findall(statement_str)
-
-        if verbose:
-            print(token_type)
-
-        # assert token_type[0] == ",<1>,"
-        # assert token_type[-1] == ",<-1>,"
         return token_type
 
-    def get_token_normalize(self, token_list, token_type):
-        '''
-            remove the comments in the source code
-            Parameters : token_list & token_type
-                         token_type == 117 -> COMMENT
-                         token_type == 118 -> LINE_COMMENT
-                         token_type == 2 --> ;
-                         token_type == 15 --> ,
-                         token_type == 34 --> .
-                         token_type == 115 --> stringLex
-                         token_type == 97 --> decimalNum
-                         token_type == 98 --> hexNum
-                         token_type == 100 --> hexLex
-                         token_type == 95 --> VersionLiteral
-                         token_type == 114 --> Identifier
-            return : normalized tokens
-        '''
-        # token_list = self.token_list
-        # token_type = self.token_type
+    @staticmethod
+    def normalize_tokens(token_list, token_type):
         assert len(token_list) == len(token_type)
-        # print str(token_list) + str(len(token_list)) + str(type(token_list))
-        # print str(token_type) + str(len(token_type)) + str(type(token_type))
+        token_list_normalized = Statement_Normalizer.execute_token_normalization(token_list, token_type)
+        return [token.lower() if not token.startswith('%%%%') else token for token in token_list_normalized]
 
+    @staticmethod
+    def execute_token_normalization(token_list, token_type):
+        assert len(token_list) == len(token_type)
         token_list_normalized = []
         for i in range(len(token_list)):
-            # handle the comment
-            if token_type[i] == ",<117>," or token_type[i] == ",<118>,":
-                pass
-            # handle the punctuations
-            elif token_type[i] == ",<2>," or token_type[i] == ",<15>," or token_type == ',<34>,':
-                pass
-                # handle the version
-            elif token_type[i] == ',<95>,':
+            token_type_spec = token_type[i]
+            if token_type_spec in [",<117>,", ",<118>,", ",<2>,", ",<15>,", ",<34>,"]:
+                continue
+            elif token_type_spec == ',<95>,':
                 token_list[i] = "VersionLiteral"
-                token_list_normalized.append(token_list[i])
-            # handle the string constant
-            elif token_type[i] == ",<115>,":
-                token_list[i] = "stringLex"
-                token_list_normalized.append(token_list[i])
-            # handle the DecimalNumber
-            elif token_type[i] == ",<97>,":
-                token_list[i] = "decimalNum"
-                token_list_normalized.append(token_list[i])
-            # handle the HexNumber
-            elif token_type[i] == ",<98>,":
-                token_list[i] = "hexNum"
-                token_list_normalized.append(token_list[i])
-            # handle the HexLiteral
-            elif token_type[i] == ",<100>,":
-                token_list[i] = "hexLex"
-                token_list_normalized.append(token_list[i])
-            # handle the identifier
-            elif token_type[i] == ",<114>,":
-                # token_list_normalized.append('%%%%' +token_list[i])
-
-                identifier_candidates = []
-                camel_case_candidates = []
-
-                camel_case_exp = '([A-Z][a-z]+)'
-                camel_case_reg = re.compile(camel_case_exp)
-                camel_case_cut = camel_case_reg.findall(token_list[i])
-
-                # print '113 Case:' + token_list[i]
-                if not camel_case_cut:
-                    camel_case_candidates.append(token_list[i])
-                else:
-                    token_list_normalized.append('%%%%' + token_list[i])
-                    remain = token_list[i]
-                    for e in camel_case_cut:
-                        remain = remain.replace(e, '')
-                    if remain:
-                        camel_case_candidates.append(remain)
-                    for e in camel_case_cut:
-                        camel_case_candidates.append(e)
-
-                for candidate in camel_case_candidates:
-                    if '_' not in candidate:
-                        identifier_candidates.append(candidate)
-                    else:
-                        under_line_cut = candidate.split('_')
-                        for e in under_line_cut:
-                            if e:
-                                identifier_candidates.append(e)
-
-                token_list_normalized.extend(identifier_candidates)
-            # other situations : normal tokens
+            elif token_type_spec in [",<115>,", ",<97>,", ",<98>,", ",<100>,"]:
+                token_list[i] = "normalizedToken"
+            elif token_type_spec == ",<114>,":
+                token_list_normalized.extend(Statement_Normalizer.handle_identifier(token_list[i]))
             else:
                 token_list_normalized.append(token_list[i])
+        return token_list_normalized
 
-        # print token_list_normalized
-        normalized_tokens = []
-        for e in token_list_normalized:
-            if not e.startswith('%%%%'):
-                e = e.lower()
-            if len(e) == 1 and (e >= 'a' and e <= 'z'):
-                e = e.replace(e, 'SV')
-            normalized_tokens.append(e)
-
-        # print normalized_tokens
-        assert len(token_list_normalized) == len(normalized_tokens)
-        return normalized_tokens
+    @staticmethod
+    def handle_identifier(identifier):
+        token_list_normalized = ['%%%%' + identifier]
+        camel_case_exp = '([A-Z][a-z]+)'
+        camel_case_reg = re.compile(camel_case_exp)
+        camel_case_cut = camel_case_reg.findall(identifier)
+        if not camel_case_cut:
+            token_list_normalized.append(identifier)
+        else:
+            remain = identifier
+            for e in camel_case_cut:
+                remain = remain.replace(e, '')
+            if remain:
+                token_list_normalized.append(remain)
+            token_list_normalized.extend(camel_case_cut)
+            token_list_normalized.extend([i for i in identifier.split('_') if i])
+        return token_list_normalized
 
 
 base_dir = './AST_Parsed/output/'
@@ -230,12 +135,9 @@ contract_addrs = os.listdir(base_dir)
 for addr in tqdm(contract_addrs):
     if addr != ".DS_Store":
         try:
-            statement_tokens_path = base_dir + addr         
-            statement_norm = Statement_Norm(statement_tokens_path)
-            statement_norm.store()
+            doc_token_dir = base_dir + addr         
+            doc_norm = Statement_Normalizer(doc_token_dir)
+            doc_norm.store()
             print("success:",addr)
-            # break
-        except Exception as ex:
-            print (ex)
+        except:
             continue
-
