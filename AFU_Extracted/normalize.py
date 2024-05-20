8f7582fe255ca6c06ba0b080055d5251b766c3cb
王希特 <53168636+hitew@users.noreply.github.com>
2024-05-20 14:17:15 +0800

    Update normalize.py

--- AFU_Extracted/normalize.py
+++ AFU_Extracted/normalize.py
@@ -1,3 +1,4 @@
+
 def normalize_doc_tokens(doc_tokens, doc_tokens_type):
     return [normalize_tokens(tokens, token_type) for tokens, token_type in zip(doc_tokens, doc_tokens_type)]
 
@@ -12,3 +13,24 @@ def normalize_tokens(token_list, token_type):
             token = "normalizedToken"
         token_list_normalized.append(token.lower() if not token.startswith('%%%%') else token)
     return token_list_normalized
+    
+def normalize_files(base_dir, output_dir):
+    for addr in tqdm(os.listdir(base_dir)):
+        if addr.endswith(".txt"):  # Assuming the documents are .txt files
+            try:
+                doc_token_dir = os.path.join(base_dir, addr)
+                linespan_list, ancestor_list, doc_list = load_document_tokens(doc_token_dir)
+                doc_tokens = extract_doc_tokens(doc_list)
+                doc_tokens_type = extract_doc_tokens_type(doc_list)
+                doc_tokens_norm = normalize_doc_tokens(doc_tokens, doc_tokens_type)
+                store_normalized_docs(linespan_list, ancestor_list, doc_tokens_norm, output_dir, addr)
+                print("Success:", addr)
+            except Exception as e:
+                print(f"Error processing {addr}: {e}")
+
+if __name__ == "__main__":
+    base_dir = './AST_Parsed/output/'
+    output_dir = './AFU_Extracted/output/norm/'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    process_files(base_dir, output_dir)
