8cc43b975c4315a949593a289ded8bdfc8442a30
王希特 <53168636+hitew@users.noreply.github.com>
2024-05-20 14:30:15 +0800

    Update combination_cg_and_ast.py

--- AFU_Extracted/combination_cg_and_ast.py
+++ AFU_Extracted/combination_cg_and_ast.py
@@ -3,6 +3,7 @@ import os
 import pickle
 import re
 from tqdm import tqdm
+
 def print_nx_network_full_info(nx_graph):
     print('====Nodes info====')
     for node, node_data in nx_graph.nodes(data=True):
@@ -12,61 +13,63 @@ def print_nx_network_full_info(nx_graph):
     for source_node, target_node, edge_data in nx_graph.edges(data=True):
         print(source_node, target_node, edge_data)
 
+def preorder_traversal(graph, source_node, ast_data, output_directory, file_prefix):
+    visited = set()  
+    stack = [source_node] 
 
+    matched_ast = []  
 
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.add(node)
+            node_info = graph.nodes[node]
+            code_lines = node_info.get('node_source_code_lines', [None, None])
 
+          
+            for line_range in range(code_lines[0], code_lines[1]+1):
+                for line in ast_data.split('\n'):
+                    if f'{line_range}_' in line:
+                        matched_ast.append(line)
 
-# with open("/home/wangxite/hitework/TEST/0x2019763bd984cce011cd9b55b0e700abe42fa6c7.sol.pkl", "rb") as f:
-#     graph = pickle.load(f)
+         
+            for _, child in graph.out_edges(node):
+                if child not in visited:
+                    stack.append(child)
+    
 
-
-
-cg_dir = './AFU_Extracted/output/cg/'
-ast_dir = './AFU_Extracted/output/norm/'
-output_dir = './AFU_Extracted/output/AFU/'
+    if matched_ast:
+        with open(os.path.join(output_directory, f'{file_prefix}_ast_preorder.txt'), 'a') as f:
+            f.write('\n'.join(matched_ast) + '\n\n') 
 
 def process_and_match_ast(cg_dir, ast_dir, output_dir):
     for filename in tqdm(os.listdir(cg_dir)):
         if filename.endswith(".pkl"):
-           
+            # Load call graph
             with open(os.path.join(cg_dir, filename), 'rb') as f:
                 cg_data = pickle.load(f)
                 graph = nx.MultiDiGraph(cg_data)
 
-            
-            ast_filename = filename.split('.')[0] + 'parse_result_normalized'
+            # Load AST file
+            ast_filename = filename.split('.')[0] + '_parse_result_normalized'
             with open(os.path.join(ast_dir, ast_filename), 'r') as f:
                 ast_data = f.read()
 
-            with open(os.path.join(output_dir, f'{filename}_ast.txt'), 'a') as f:
-                f.write(ast_data + '\n')
-            for source_node, target_node, edge_data in graph.edges(data=True):
-             
-                source_node_info = graph.nodes[source_node]
-                target_node_info = graph.nodes[target_node]
+            # Find a source node to start preorder traversal, assuming graph is a tree
+            # This could be the first node with no incoming edges, or root nodes based on domain knowledge
+            for node in graph.nodes:
+                if graph.in_degree(node) == 0:  # Found a root node
+                    preorder_traversal(graph, node, ast_data, output_dir, filename.split('.')[0])
+                    break  # Remove this break if you want to start traversal from all roots
 
-              
-                source_code_lines = source_node_info['node_source_code_lines']
-                target_code_lines = target_node_info['node_source_code_lines']
-
-                
-                matched_ast = []
-                for line_range in range(source_code_lines[0], source_code_lines[1]+1):
-                    for line in ast_data.split('\n'):
-                        if f'{line_range}_' in line:
-                            matched_ast.append(line)
-
-                for line_range in range(target_code_lines[0], target_code_lines[1]+1):
-                    for line in ast_data.split('\n'):
-                        if f'{line_range}_' in line:
-                            matched_ast.append(line)
-
-                if matched_ast:
-                  
-                    
-                    
-                    # Write matched_ast information
-                    with open(os.path.join(output_dir, f'{filename}_ast.txt'), 'a') as f:
-                        f.write('\n'.join(matched_ast) + '\n')
-
-process_and_match_ast(cg_dir, ast_dir, output_dir)
+if __name__ == "__main__":
+    cg_dir = './AFU_Extracted/output/cg/'
+    ast_dir = './AFU_Extracted/output/norm/'
+    output_dir = './AFU_Extracted/output/AFU/'
+    
+    # Check if output directory exists, if not create it
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    # Process and match AST with call graph
+    process_and_match_ast(cg_dir, ast_dir, output_dir)
